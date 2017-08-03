6bb1478892aae42e78c8d2eb89b66f19315493a9
Kus Cámara <kcmr@users.noreply.github.com>
2017-08-03 21:57:33 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+let maintenance = false;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+// register middleware
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: Method: ${req.method} Path: ${req.url}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+if (maintenance) {
+  app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+  });
+}
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    title: 'Home page',
+    welcomeMessage: 'Welcome to some website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'About page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Sirviendo nosequé');
+});
